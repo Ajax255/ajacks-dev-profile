5a02d3ce4fb141eda46e00c46993c0b9d2be0a92
Anthony <anthony.jackson.002@gmail.com>
2022-02-19 23:01:57 -0500

    added routes to routers and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,40 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../components/Home.vue'
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Blog from '../views/Blog.vue'
+import Contact from '../views/Contact.vue'
+import Projects from '../views/Projects.vue'
+import Work from '../views/Work.vue'
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
   },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects,
+  },
+  {
+    path: '/work',
+    name: 'Work',
+    component: Work,
+  },
+  { path: '/:pathMatch(.*)*', component: Home },
 ]
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 export default router
---
+++ src/views/Contact.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+</script>
+
+<template>
+  <div>Contact!</div>
+</template>
+
+<style lang="scss" scoped></style>
